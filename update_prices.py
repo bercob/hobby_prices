a1c8fadc5b059e2a6add057651931e124df29db8
berco <bercob@gmail.com>
2018-11-04 15:55:12 +0100

    get product list - WIP

--- update_prices.py
+++ update_prices.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import optparse, requests, logging, signal, sys, urllib, urllib3, urlparse
+import optparse, requests, logging, signal, sys, urllib, urllib3, urlparse, traceback
 from bs4 import BeautifulSoup
 from ConfigParser import SafeConfigParser
 
-CREDENTIALS_FILE_PATH = "ini/credentials.ini"
 VERSION = "1.0.0"
 AUTHOR = "Balogh Peter <bercob@gmail.com>"
-LOG_FILE_PATH = "log/update_price.log"
+
 SEARCH_URL = "https://www.heureka.sk"
 SEARCH_VAR = "h[fraze]"
-INTERNAL_LOGIN_URL = "https://www.hobbynemeth.sk/admin/index.php?route=api/login"
 MY_SHOP_NAME = "HOBBY-Németh"
 PRODUCT_LINK_SELECTOR = "div.product a"
 PRODUCT_PRICE_SELECTOR = "div.shopspr.bottom a.pricen"
@@ -25,8 +23,8 @@ def signal_handler(signal, frame):
 def help(parser):
 	parser.print_help()
 
-def set_logging():
-	logging.basicConfig(filename = LOG_FILE_PATH, level = logging.INFO, format = "%(asctime)s %(message)s")
+def set_logging(ini_parser):
+	logging.basicConfig(filename = ini_parser.get("logging", "log_file_path"), level = logging.INFO, format = "%(asctime)s %(message)s")
 	sh = logging.StreamHandler(sys.stdout)
 	sh.setFormatter(logging.Formatter("%(asctime)s %(message)s"))
 	logging.getLogger().addHandler(sh)
@@ -44,8 +42,8 @@ def parse_arguments(m_args):
 		sys.exit(0)
 	
 	if len(args) == 0:
-		parser.error('product(s) is required')
-	
+		parser.error('ini file path is required')
+
 	return options, args
 
 def main(m_args=None):
@@ -54,60 +52,66 @@ def main(m_args=None):
 
 	urllib3.disable_warnings()
 
-	set_logging()
-
 	(options, args) = parse_arguments(m_args)
+
+	ini_parser = SafeConfigParser()
+	ini_parser.read(args[0])
+
+	set_logging(ini_parser)
 
 	logging.info("starting")
 
-	ini_parser = SafeConfigParser()
-	ini_parser.read(CREDENTIALS_FILE_PATH)
+	credentials_ini_parser = SafeConfigParser()
+	credentials_ini_parser.read(ini_parser.get("credentials", "ini_file_path"))
 
 	http = urllib3.PoolManager()
 
-	for product in args:
-		try:
-			url = "%s/?%s" % (SEARCH_URL, urllib.urlencode({ SEARCH_VAR: product }))
+	try:
 
-			logging.info("opening %s" % url)
+		with requests.Session() as s:
+		    login_response = s.post(ini_parser.get("urls", "api_url") + ini_parser.get("pages", "login_page"), data = { "username": credentials_ini_parser.get("authentication", "user_name"), "password": credentials_ini_parser.get("authentication", "password") })
+		    token = urlparse.parse_qs(urlparse.urlparse(login_response.url).query)["token"][0]
+		    
+		    product_list_response = s.get(ini_parser.get("urls", "api_url") + ini_parser.get("pages", "product_list_page"), params = { "token": token })
 
-			r = http.request("GET", url)
+		    print product_list_response
 
-			bs = BeautifulSoup(r.data, features = "html.parser")
+	# for product in args:
+	# 	try:
+	# 		url = "%s/?%s" % (SEARCH_URL, urllib.urlencode({ SEARCH_VAR: product }))
 
-			if not bs.select(PRODUCT_FOUND_SELECTOR):
-				logging.info("product %s has not found" % product)
-				continue
+	# 		logging.info("opening %s" % url)
 
-			product_url = bs.select(PRODUCT_LINK_SELECTOR)[0]["href"]
+	# 		r = http.request("GET", url)
 
-			logging.info("opening %s" % product_url)
+	# 		bs = BeautifulSoup(r.data, features = "html.parser")
 
-			r = http.request("GET", product_url)		
+	# 		if not bs.select(PRODUCT_FOUND_SELECTOR):
+	# 			logging.info("product %s has not found" % product)
+	# 			continue
+
+	# 		product_url = bs.select(PRODUCT_LINK_SELECTOR)[0]["href"]
+
+	# 		logging.info("opening %s" % product_url)
+
+	# 		r = http.request("GET", product_url)		
 		
-			bs = BeautifulSoup(r.data, features = "html.parser")
+	# 		bs = BeautifulSoup(r.data, features = "html.parser")
 			
-			best_price_array = bs.select(PRODUCT_PRICE_SELECTOR)[0].contents[0].rsplit(" ", 1)
-			best_price = float(best_price_array[0].replace(" ", "").replace(",","."))
-			best_price_currency = best_price_array[1]
-			best_shop_name = bs.select(SHOP_NAME_SELECTOR)[0].contents[0].strip()
-			product_name = bs.select(PRODUCT_NAME_SELECTOR)[0].contents[0]
-			my_offer_exist = any(MY_SHOP_NAME in shop_name for shop_name in [tag.contents[0] for tag in bs.select(SHOP_NAME_SELECTOR)])
+	# 		best_price_array = bs.select(PRODUCT_PRICE_SELECTOR)[0].contents[0].rsplit(" ", 1)
+	# 		best_price = float(best_price_array[0].replace(" ", "").replace(",","."))
+	# 		best_price_currency = best_price_array[1]
+	# 		best_shop_name = bs.select(SHOP_NAME_SELECTOR)[0].contents[0].strip()
+	# 		product_name = bs.select(PRODUCT_NAME_SELECTOR)[0].contents[0]
+	# 		my_offer_exist = any(MY_SHOP_NAME in shop_name for shop_name in [tag.contents[0] for tag in bs.select(SHOP_NAME_SELECTOR)])
 			
-			logging.info("best price for %s (%s) is %.2f %s from %s" % (product, product_name, best_price, best_price_currency, best_shop_name))
+	# 		logging.info("best price for %s (%s) is %.2f %s from %s" % (product, product_name, best_price, best_price_currency, best_shop_name))
 
-			if my_offer_exist and best_shop_name != MY_SHOP_NAME:
-				logging.info("price changing")
+	# 		if my_offer_exist and best_shop_name != MY_SHOP_NAME:
+	# 			logging.info("price changing")
 
-				with requests.Session() as s:
-				    p = s.post(INTERNAL_LOGIN_URL, data = { "username": ini_parser.get("authentication", "user_name"), "password": ini_parser.get("authentication", "password") })
-				    token = urlparse.parse_qs(urlparse.urlparse(p.url).query)["token"][0]
-				    
-				    logging.info("internal product ID is %s" % internal_product_id)
-
-		except:
-			import traceback
-			logging.error("error: %s" % traceback.format_exc(sys.exc_info()[2]))
+	except:
+		logging.error("error: %s" % traceback.format_exc(sys.exc_info()[2]))
 
 	logging.info("end")
 
